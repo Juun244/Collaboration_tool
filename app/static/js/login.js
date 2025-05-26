2c4df471d3140e2ecd319ede45824485c47534de
Hongbae32 <shb5768123@naver.com>
2025-05-26 21:08:24 +0900

    modify card
    modify a card

--- app/static/js/login.js
+++
@@ -1,9 +0,0 @@
-function addRememberParam(link) {
-    const rememberCheckbox = document.getElementById('remember');
-    if (rememberCheckbox.checked) {
-      const url = new URL(link.href);
-      url.searchParams.set('remember', '1');
-      link.href = url.toString();
-    }
-    return true;
-  }
